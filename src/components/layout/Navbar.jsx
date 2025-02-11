6050dc0f7924922e5c626e4f5a7fff06a7b2059f
Ahmed KACEM <a.kacem@gexpertise.fr>
2025-02-11 17:06:19 +0100

    fix : responsive navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,7 +3,8 @@ import { FaHome, FaBell, FaUser, FaGlobe } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-900 p-4 flex justify-end gap-x-52 items-center  ">
+    <nav className="bg-gray-900 p-4 flex justify-end  items-center  ">
+      <div className="flex lg:w-1/4 md:w-1/3 w-2/4 justify-between">
 
         <div className="flex items-center gap-x-4">
           <Link to="/" className="text-white text-2xl">
@@ -22,6 +23,7 @@ const Navbar = () => {
             <span className="text-sm">FR</span> <FaGlobe />
           </Link>
         </div>
+      </div>
     </nav>
   );
 };
